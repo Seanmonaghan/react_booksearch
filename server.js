f094b52e732f8ff5c10e354ef762bb48612df93e
Sean Monaghan <smonagha@conncoll.edu>
2021-06-22 11:01:51 -0400

    added procfile

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const path = require('path');
+const cors = require('cors');
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
+
+require('./models/book');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,15 +21,18 @@ mongoose.connection.on('connected', () => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
 }
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/reactBooks", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-    // useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
