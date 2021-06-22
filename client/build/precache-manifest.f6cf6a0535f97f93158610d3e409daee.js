d7b031a8135db1e49bb1e2b001af0e683a58c76b
Sean Monaghan <smonagha@conncoll.edu>
2021-06-21 21:56:16 -0400

    added build folder

---
+++ client/build/precache-manifest.f6cf6a0535f97f93158610d3e409daee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2463e02332357d6aeffaea259228ef54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "183d2653f3113bf3df84",
+    "url": "/static/css/main.103fa33c.chunk.css"
+  },
+  {
+    "revision": "30c70797d0dfaa8fbb1c",
+    "url": "/static/js/2.28732be7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.28732be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183d2653f3113bf3df84",
+    "url": "/static/js/main.918a49b7.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "b34dfb8c61d086b55c7e2910a90fe45a",
+    "url": "/static/media/bannerBooks.b34dfb8c.jpg"
+  }
+]);
