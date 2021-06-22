011da2aa00e23c2a298fc3b62d2330ec67ea1909
Sean Monaghan <smonagha@conncoll.edu>
2021-06-22 13:36:23 -0400

    resolved proxy bug

--- server.js
+++ server.js
@@ -1,43 +1,28 @@
+  
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require('path');
-const cors = require('cors');
-
-require("dotenv").config();
-
-const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 3001;
 
-require('./models/book');
-
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-mongoose.connection.on('connected', () => {
-  console.log("Mongoose is connected")
-})
-
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
 }
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactBooks", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
+// Add routes, both API and view
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/myReactreadingList"
+);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
