abff5f816749c3bfd3e93bb428998401303ef0e1
Sean Monaghan <smonagha@conncoll.edu>
2021-06-22 14:50:43 -0400

    remove pathing to try and help with saved book issues on deployment

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
   
 const express = require("express");
-const path = require('path');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -12,16 +11,13 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/myReactreadingList", {
+  process.env.MONGODB_URI || "mongodb://localhost/react-books", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
